017a45178ad1e5cdbfbbea3329945f152c2fc19d
Francis Okonkwo <francis.okonkwo@powerreviews.com>
2016-10-18 16:45:58 -0500

    Add SASS and CSS Modules
    Adds sass-loader and node-sass dependencies. Enable CSS Modules in web
    pack configs.
    Heavily inspired by
    https://github.com/kitze/create-react-app/blob/master/packages/react-scr
    ipts/config/customizers.js

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -149,8 +149,13 @@ module.exports = {
         // Webpack 1.x uses Uglify plugin as a signal to minify *all* the assets
         // including CSS. This is confusing and will be removed in Webpack 2:
         // https://github.com/webpack/webpack/issues/283
-        loader: ExtractTextPlugin.extract('style', 'css?-autoprefixer!postcss')
+        loader: ExtractTextPlugin.extract('style', 'css?modules&importLoaders=1&camelCase&-autoprefixer!postcss')
         // Note: this won't work without `new ExtractTextPlugin()` in `plugins`.
+      },
+      // "sass" loader extraction
+      {
+        test: /(\.scss|\.sass)$/,
+        loader: ExtractTextPlugin.extract('style', 'css?-autoprefixer!postcss!sass')
       },
       // JSON is not enabled by default in Webpack but both Node and Browserify
       // allow it implicitly so we also enable it.
